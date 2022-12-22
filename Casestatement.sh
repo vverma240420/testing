f3f71a1c56913d8247b5c3e8cc686b512f802aae
Vivek Verma <vverma240420@gmail.com>
2022-12-22 20:39:48 +0530

    Give

---
+++ Casestatement.sh
@@ -0,0 +1,21 @@
+randomCheck=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+case $randomCheck in 
+0)
+echo "Employee is absent";
+workingHour=0;
+;;
+1)
+echo "Employee is present";
+workingHour=8;
+;;
+2)
+echo "Employee is working as part time";
+workingHour=4;
+;;
+*)
+echo "not matching";
+esac
+salary=$(($perHourSalary * $workingHour));
+echo "Employee has earned $salary $ today";
---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+public class Helloworld 
+{
+public static void main(String[] args)
+{
+while(true)
+{
+System.out.println("Hello World");
+try{
+Thread.sleep(50000);
+}
+catch (Exception ex) {}
+}
+}
+}
